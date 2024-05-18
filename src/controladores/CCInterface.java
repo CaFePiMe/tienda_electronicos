1b1ec24ad88e4ad6e2ae17986757058702f5f810
CaFePiMe <159438510+CaFePiMe@users.noreply.github.com>
2024-05-18 08:55:10 -0400

    controladorrestaurado

--- src/controladores/CCInterface.java
+++ src/controladores/CCInterface.java
@@ -1,5 +1,12 @@
 package controladores;
 
+import java.sql.ResultSet;
+
 public interface CCInterface {
+	public ResultSet getRegistros(String nombreTabla, String[] campos, String[] columnaId, Object[] id);
+
+	public int actualizarRegistro(String nomTabla, String[] cnls, String columnaId, Object id);
+
+	public int agregarRegistro(String nombreTabla, String[] campos, Object[] valores);
 
 }
